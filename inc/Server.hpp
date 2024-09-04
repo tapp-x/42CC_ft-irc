a3bb10ba44c67586a40a974b2e943a0b0da70a74
theoapp <theoappourchaux62@gmail.com>
2024-09-04 20:02:48 +0200

    chore: add fundamental functions of server class

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: theoappourchaux <theoappourchaux@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:49:15 by tappourc          #+#    #+#             */
-/*   Updated: 2024/08/29 11:18:06 by tappourc         ###   ########.fr       */
+/*   Updated: 2024/09/04 19:49:59 by theoappourc      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,38 @@
 class Server {
 
 	private:
-		int			port;
-		int			client_max;
-		int			sock;
+		int						port;
+		int						client_max;
+		int						sock_serv;
 
-		std::string	password;
-		std::string	hostname;
+		std::string				password;
+		std::string				hostname;
+
+		struct sockaddr_in		addr;
+
+		// need to create other class
+		// std::vector<Client>		clients;
+		// std::vector<Channel>	channels;
 
 	public:
+		// BASICS
 		Server();
+		~Server();		
 		Server(const Server &server);
 		Server	&operator=(const Server &server);
-		~Server();
+
+		// SETTERS
+		void			init(int port, int maxclient, std::string password, std::string hostname);
+		void			set_port(int port);
+		void			set_clientmax(int port);
+		void			set_sockserv();
+		
+		// GETTERS
+		int				get_port();
+		int				get_sockserv();
+		std::string		get_password();
+		std::string		get_hostname();
+		
 }
 
 #endif
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -3,10 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: theoappourchaux <theoappourchaux@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:49:41 by tappourc          #+#    #+#             */
-/*   Updated: 2024/08/29 10:49:42 by tappourc         ###   ########.fr       */
+/*   Updated: 2024/09/04 19:54:50 by theoappourc      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Server.hpp"
+
+Server::Server(){
+	this->port = 0;
+	this->client_max = 0;
+	this->sock_serv = -1;
+}
+
+Server::~Server(){}
+
+Server::Server(const Server &server) {
+	*this = server;
+}
+
+Server &Server::operator=(const Server &server) {
+	if (this != &server) {
+		port = server.port;
+		client_max = server.client_max;
+		sock_serv = server.sock_serv;
+		addr = server.addr;
+		// need to create other class
+		// clients = server.clients;
+		// channels = server.channels;
+	}
+	return (*this);
+}
+
+// ALL SETTERS
+void	Server::init(int port, int maxclient, std::string password, std::string hostname){
+	this->port = port;
+	this->client_max = maxclient;
+	this->password = password;
+	this->hostname = hostname;
+}
+
+void	Server::set_port(int port){
+	this->port = port;
+}
+
+void	Server::set_clientmax(int clientmax){
+	this->client_max = clientmax;
+}
+
+// void	Server::set_sockserv(){
+	
+// }
+
+// ALL GETTERS
+int	Server::get_port() {
+	return (this->port);	
+}
+
+int	Server::get_sockserv() {
+	return (this->sock_serv);	
+}
+
+std::string	Server::get_password() {
+	return (this->password);	
+}
+
+std::string	Server::get_hostname() {
+	return (this->hostname);	
+}
