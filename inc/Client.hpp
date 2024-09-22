90fe7cba5972b052d4cac93a52e2fdf83192cdde
theoapp <116431821+TheoAppourchaux@users.noreply.github.com>
2024-09-22 19:15:54 +0200

    chore: start client class

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -6,11 +6,38 @@
 /*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:54:23 by tappourc          #+#    #+#             */
-/*   Updated: 2024/08/29 10:54:33 by tappourc         ###   ########.fr       */
+/*   Updated: 2024/09/19 14:23:11 by tappourc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
+
+#include "irc.hpp"
+
+class Client {
+	private:
+		int			_fd;
+
+		std::string _username;
+		std::string _nickname;
+	public:
+	//BASICS
+		Client();
+		Client(int fd, std::string username, std::string nickname);
+		~Client();
+		Client(Client const &src);
+		Client &operator=(Client const &src);
+	
+	//GETTERS
+		std::string	get_username();
+		std::string	get_nickname();
+		int			get_fd();
+	//SETTERS
+		void		set_username(std::string username);
+		void		set_nickname(std::string nickname);
+		void		set_fd(int fd);
+	// METHODS
+};
 
 #endif
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -6,7 +6,45 @@
 /*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:54:46 by tappourc          #+#    #+#             */
-/*   Updated: 2024/08/29 10:54:47 by tappourc         ###   ########.fr       */
+/*   Updated: 2024/09/19 14:21:52 by tappourc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Client.hpp"
+
+// BASICS
+Client::~Client() {}
+
+Client::Client() {
+	this->_fd = -1;
+	this->_username = "";
+	this->_nickname = "";
+}
+
+Client::Client(int fd, std::string username, std::string nickname){
+	this->_fd = fd;
+	this->_username = username;
+	this->_nickname = nickname;
+}
+
+// GETTERS
+std::string	Client::get_username() {
+	return (this->_username);
+}
+std::string	Client::get_nickname() {
+	return (this->_nickname);
+}
+int			Client::get_fd() {
+	return (this->_fd);
+}
+
+// SETTERS
+void	Client::set_username(std::string username) {
+	this->_username = username;
+}
+void	Client::set_nickname(std::string nickname) {
+	this->_nickname = nickname;
+}
+void	Client::set_fd(int fd) {
+	this->_fd = fd;
+}
