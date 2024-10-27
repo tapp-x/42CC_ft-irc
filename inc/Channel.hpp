c3b9283139ada42b18958ccc421f9621e39592df
theoapp <116431821+TheoAppourchaux@users.noreply.github.com>
2024-10-27 11:45:03 +0100

    start to implement the Channel class (value/get/set)

---
+++ inc/Channel.hpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 23:54:44 by tappourc          #+#    #+#             */
+/*   Updated: 2024/10/27 11:44:01 by tappourc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include <vector>
+#include <string>
+#include "Client.hpp"
+
+class Channel {
+private:
+	// MEMBERS
+		std::vector<Client *>		_clients;
+		std::vector<Client *>		_admins;
+		std::vector<std::string>	_invited;
+
+	// ATTRIBUTES
+		std::string					_name;
+		std::string					_topic;
+		std::string					_key;
+
+	// PARAMETERS
+		size_t						_limit;
+		bool						_inviteOnly;
+		bool						_topicRestricted;
+		bool						_limited;
+
+public:
+	// BASICS
+		Channel(const std::string &name);
+		~Channel();
+		Channel(const Channel &other);
+		Channel &operator=(const Channel &other);
+
+	// GETTERS
+		std::string					getName() const;
+		std::string					getTopic() const;
+		std::string					getKey() const;
+		size_t						getLimit() const;
+		std::vector<Client *>		getClients() const;
+		std::vector<Client *>		getAdmins() const;
+		std::vector<std::string>	getInvited() const;
+		bool						isTopicRestricted() const;
+		bool						isLimited() const;
+		bool						isInviteOnly() const;
+
+	// SETTERS
+		void						setInviteOnly(bool inviteOnly);
+		void						setTopicRestricted(bool topicRestricted);
+		void						setTopic(const std::string &topic);
+		void						setKey(const std::string &key);
+		void						setLimited(bool limited);
+		void						setLimit(size_t limit);
+
+	// METHODS
+		void						addClient(Client *client);
+		void						removeClient(Client *client);
+
+	// ADMIN METHODS
+		void						addAdmin(Client *client);
+		void						removeAdmin(Client *client);
+
+	// INVITE METHODS
+		void						inviteClient(const std::string &nickname);
+		void						removeInvited(const std::string &nickname);
+};
+
+#endif
---
+++ srcs/Channel.cpp
@@ -0,0 +1,151 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 23:54:34 by tappourc          #+#    #+#             */
+/*   Updated: 2024/10/27 11:43:52 by tappourc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Channel.hpp"
+#include <algorithm>
+#include <iostream>
+
+// BASICS
+Channel::Channel(const std::string &name)
+	: _name(name), _inviteOnly(false), _topicRestricted(false), _limited(false), _limit(0) {}
+
+Channel::~Channel() {}
+
+Channel::Channel(const Channel &other) {
+	*this = other;
+}
+
+Channel &Channel::operator=(const Channel &other) {
+	if (this != &other) {
+		_name = other._name;
+		_inviteOnly = other._inviteOnly;
+		_topicRestricted = other._topicRestricted;
+		_topic = other._topic;
+		_key = other._key;
+		_limited = other._limited;
+		_limit = other._limit;
+		_clients = other._clients;
+		_admins = other._admins;
+		_invited = other._invited;
+	}
+	return *this;
+}
+
+// GETTERS
+std::string Channel::getName() const {
+	return _name;
+}
+
+bool Channel::isInviteOnly() const {
+	return _inviteOnly;
+}
+
+bool Channel::isTopicRestricted() const {
+	return _topicRestricted;
+}
+
+std::string Channel::getTopic() const {
+	return _topic;
+}
+
+std::string Channel::getKey() const {
+	return _key;
+}
+
+bool Channel::isLimited() const {
+	return _limited;
+}
+
+size_t Channel::getLimit() const {
+	return _limit;
+}
+
+std::vector<Client *> Channel::getClients() const {
+	return _clients;
+}
+
+std::vector<Client *> Channel::getAdmins() const {
+	return _admins;
+}
+
+std::vector<std::string> Channel::getInvited() const {
+	return _invited;
+}
+
+// SETTERS
+void Channel::setInviteOnly(bool inviteOnly) {
+	_inviteOnly = inviteOnly;
+}
+
+void Channel::setTopicRestricted(bool topicRestricted) {
+	_topicRestricted = topicRestricted;
+}
+
+void Channel::setTopic(const std::string &topic) {
+	_topic = topic;
+}
+
+void Channel::setKey(const std::string &key) {
+	_key = key;
+}
+
+void Channel::setLimited(bool limited) {
+	_limited = limited;
+}
+
+void Channel::setLimit(size_t limit) {
+	_limit = limit;
+}
+
+// METHODS
+void Channel::addClient(Client *client) {
+	if (_limited && _clients.size() >= _limit) {
+		std::cerr << "Channel limit reached. Cannot add more clients." << std::endl;
+		return;
+	}
+	_clients.push_back(client);
+	std::cout << "Client " << client->get_fd() << " added to channel " << _name << std::endl;
+}
+
+void Channel::removeClient(Client *client) {
+	std::vector<Client *>::iterator it = std::find(_clients.begin(), _clients.end(), client);
+	if (it != _clients.end()) {
+		_clients.erase(it);
+		std::cout << "Client " << client->get_fd() << " removed from channel " << _name << std::endl;
+	}
+}
+
+void Channel::addAdmin(Client *client) {
+	_admins.push_back(client);
+	std::cout << "Client " << client->get_fd() << " added as admin to channel " << _name << std::endl;
+}
+
+void Channel::removeAdmin(Client *client) {
+	std::vector<Client *>::iterator it = std::find(_admins.begin(), _admins.end(), client);
+	if (it != _admins.end()) {
+		_admins.erase(it);
+		std::cout << "Client " << client->get_fd() << " removed as admin from channel " << _name << std::endl;
+	}
+}
+
+void Channel::inviteClient(const std::string &nickname) {
+	_invited.push_back(nickname);
+	std::cout << "Client " << nickname << " invited to channel " << _name << std::endl;
+}
+
+void Channel::removeInvited(const std::string &nickname) {
+	std::vector<std::string>::iterator it = std::find(_invited.begin(), _invited.end(), nickname);
+	if (it != _invited.end()) {
+		_invited.erase(it);
+		std::cout << "Client " << nickname << " removed from invited list of channel " << _name << std::endl;
+	}
+}
